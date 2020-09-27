9876068b86c66c4a774f8ab705828d3929890144
jeffrey d sun <jeffreydsun90@gmail.com>
2020-09-27 12:54:01 -0700

    solution for js0-1

---
+++ js-0/1.js
@@ -0,0 +1,12 @@
+/*
+Write a function that takes in 2 numbers and returns their sum.
+
+Here's how another developer might use your function:
+
+solution(5,9) // Should return 14
+solution(4,1) // Should return 5
+*/
+
+const solution = (num1, num2) => num1 + num2
+
+console.log(solution(9, 2)) // 11

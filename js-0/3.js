977bf393da1c60388f78adec4bd9a1a5799992f7
jeffrey d sun <jeffreydsun90@gmail.com>
2020-09-27 13:04:18 -0700

    add solution for js0-3

---
+++ js-0/3.js
@@ -0,0 +1,14 @@
+/*
+Write a function that takes in a number and returns true if that number is greater than 5. Otherwise, return false.
+
+Here's how another developer might use your function:
+
+solution(9) // Should return true
+solution(4) // Should return false
+*/
+
+const solution = (num) => num > 5
+
+console.log(solution(5)) // false
+console.log(solution(2)) // false
+console.log(solution(6)) // true

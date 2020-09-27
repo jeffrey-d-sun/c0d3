d1163e400868fda8fa949c0ce9e7295176e7606e
jeffrey d sun <jeffreydsun90@gmail.com>
2020-09-27 13:20:09 -0700

    add solution for js0-9

---
+++ js-0/9.js
@@ -0,0 +1,17 @@
+/*
+Write a function that takes in 2 numbers, return a function that takes in a number that returns the sum of all 3 numbers when called. Example:
+
+const a = solution(1,2); // a is a function
+// a(1) returns 4 because 1+2+1
+// a(5) returns 8 because 1 + 2 + 5 
+// a(2) returns 5 because 1 + 2 + 2
+*/
+
+const solution = (num1, num2) => {
+  return (num3) => {
+    return num1 + num2 + num3
+  }
+}
+
+const cb = solution(1, 2)
+console.log(cb(3)) // 6

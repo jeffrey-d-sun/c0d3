eaca26a28203605465b39421cbef3c80ae4b523f
jeffrey d sun <jeffreydsun90@gmail.com>
2020-09-27 13:12:15 -0700

    add solution for js0-6

---
+++ js-0/6.js
@@ -0,0 +1,13 @@
+/*
+Write a function that takes in 2 numbers and returns true if the first number is greater than the second, false otherwise.
+
+Here's how another developer might use your function:
+
+solution(5,9) // Should return false
+solution(4,1) // Should return true
+*/
+
+const solution = (first, second) => first > second
+
+console.log(solution(5, 9)) // false
+console.log(solution(4, 1)) // true

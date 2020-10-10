c0aa074e3210409d7d4eca428d0909729344b7cf
jeffrey d sun <jeffreydsun90@gmail.com>
2020-10-10 13:21:13 -0700

    Add solution for cMap

---
+++ js-2/cMap.js
@@ -0,0 +1,28 @@
+/*
+Write a function called solution that replicates Array.prototype.map function and call it cMap
+
+Callback takes 3 input parameters, element, index and original array. documentation
+
+result = [5,8,7].cMap( (e, i, arr) => {
+  console.log(e, i, arr)
+  return e + i
+})
+// console.log will be called 3 times:
+//    5, 0, original array
+//    8, 1, original array
+//    7, 2, original array
+// result will be [5, 9, 9]
+*/
+
+const solution = () => {
+  Array.prototype.cMap = function(cb, array = [], i = 0) {
+    if (i === this.length) return array
+    array.push(cb(this[i], i, this))
+    return this.cMap(cb, array, i+1)
+  }
+}
+
+solution()
+const arr = [1, 2, 3]
+const res = arr.cMap((num) => num + 1)
+console.log(res) // [2, 3, 4]
